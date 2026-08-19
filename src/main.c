3fcec6036ead78f7a6c52ae009260c4a138632b2
SwarnaJaswanth-2007 <swarnajaswanth573@gmail.com>
2026-08-19 14:42:55 +0530

    Milestone-2.2

--- src/main.c
+++ src/main.c
@@ -1,80 +1,59 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
+#include <stdlib.h> 
 #include <string.h>
-#include <readline/readline.h>
 #include <readline/history.h>
-
-// Function to print command history
-void print_history(void)
-{
-    HIST_ENTRY **hist = history_list();
-
-    if (hist == NULL)
-    {
-        printf("No commands in history.\n");
-        return;
-    }
-
-    for (int i = 0; hist[i] != NULL; i++)
-    {
-        printf("%d %s\n", i + 1, hist[i]->line);
-    }
-}
-
+#include <readline/readline.h>
+#include "token.h"
+#include "lexer.h"
+#include "parser.h"
+#include "expand.h"
 int main(void)
 {
-    // Welcome Banner
+    // Display a welcome banner when the shell starts
     printf("=====================================\n");
-    printf("          Shellforge\n");
-    printf("   A Unix Style Shell written in C\n");
+    printf("      Shellforge \n");
+    printf(" A Unix Style Shell written in C\n");
     printf("=====================================\n");
 
-    // Initialize history
-    using_history();
-
-    char *line;
+  token_list_t tokens;
+ pipeline_t pipeline;
+ 
+ char *line;
 
     while (1)
     {
         line = readline("shellforge$ ");
-
         if (line == NULL)
         {
             printf("\nGoodbye!\n");
             break;
         }
-
-        // Ignore empty input
         if (strlen(line) == 0)
         {
             free(line);
             continue;
         }
 
-        // Exit command
+         add_history(line);
+	lexer(line, &tokens);
+        token_print(&tokens);
+        
+
+	if(parser(&tokens, &pipeline))
+	{
+		expand_variables(&pipeline);
+    		pipeline_print(&pipeline);
+	}
+        
+
+
         if (strcmp(line, "exit") == 0)
         {
             free(line);
             printf("Exiting...\n");
             break;
         }
-
-        // Save command in history
-        add_history(line);
-
-        // Show history
-        if (strcmp(line, "history") == 0)
-        {
-            print_history();
-            free(line);
-            continue;
-        }
-
-        // Echo user input
-        printf("YOU ENTERED : %s\n", line);
-
-        free(line);
-    }
-
+	free(line);
+    }    
     return 0;
 }
